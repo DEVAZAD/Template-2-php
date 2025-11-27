03221214aa60d48ac9fa16c59ee4be019e288fed
devazad <96633064+DEVAZAD@users.noreply.github.com>
2025-11-27 11:29:20 +0530

    Single page

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<header id="head">
+    <nav class="navbar bread-slices">
+        <div class="head-container  flex">
+            <div class="logo">
+                <a href="/">
+                    <img src="/assets/logo.png" alt="ABCEL Logo" class="logo-img">
+                </a>
+            </div>
+            <div class="hamburger">&#9776;</div>
+            <ul class="nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="pages/about.php">About Us</a></li>
+                <li><a href="pages/programs.php">Programs</a></li>
+                <li><a href="pages/contact.php">Connect</a></li>
+                <li><a href="pages/login.php">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
